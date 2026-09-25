ea9c1c717f82dc220a2c2175d43d170e5e204138
Savannah Darling <savannahdarling07@gmail.com>
2026-09-25 13:09:09 -0600

    Delete toolbox.js

--- toolbox.js
+++
@@ -1,8 +0,0 @@
-
-function copyText(id, btn){
-  const text=document.getElementById(id).innerText;
-  navigator.clipboard.writeText(text).then(()=>{
-    const old=btn.innerText; btn.innerText="Copied";
-    setTimeout(()=>btn.innerText=old,1000);
-  });
-}
